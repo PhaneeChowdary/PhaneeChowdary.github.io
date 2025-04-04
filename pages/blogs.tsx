593b9691d12e29e90dc3b49acb925738c7cc3332
PhaneeChowdary <phaneendrababugunturu4393@gmail.com>
2025-04-04 16:35:53 -0400

    Website

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -6,6 +6,13 @@ import Footer from "../components/Footer";
 import { blogs } from "../utils/blogs";
 
 const BlogsPage = () => {
+    // Sort blogs by date_created in descending order
+    const latestBlogs = [...blogs].sort(
+        (a, b) =>
+            new Date(b.date_created).getTime() -
+            new Date(a.date_created).getTime()
+    );
+
     return (
         <Layout>
             <Head>
@@ -14,14 +21,21 @@ const BlogsPage = () => {
             <Header />
             <section id="blogs" className="my-10">
                 <div className="space-y-4">
-                    {blogs.map((blog) => (
+                    {latestBlogs.map((blog) => (
                         <div
                             key={blog.id}
                             className="border p-4 rounded-lg transform transition-transform duration-300 hover:-translate-y-1"
                         >
-                            <a href={blog.link} target="_blank" rel="noopener noreferrer" className="block hover:no-underline">
+                            <a
+                                href={blog.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="block hover:no-underline"
+                            >
                                 <h3 className="text-xl">{blog.name}</h3>
-                                <p className="text-gray-600">{blog.description}</p>
+                                <p className="text-gray-600">
+                                    {blog.description}
+                                </p>
                             </a>
                         </div>
                     ))}
